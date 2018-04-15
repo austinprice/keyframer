76e576ecaba6e3bf0a77e7eac77383fa032fee84
Austin Price <priceaustinw@gmail.com>
2018-04-14 20:43:48 -0400

    Added a color picker for the background color field

--- app/components/text-input-color.js
+++ app/components/text-input-color.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
+import { observer } from '@ember/object';
 
 export default Ember.Component.extend({
+
+  triggerRewrite: observer('value', function() {
+    this.get('keypressAction')();
+  }),
+
   actions: {
     keypressAction() {
       this.get('keypressAction')();
