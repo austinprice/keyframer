a2164e92ce2c55213a9f4a5ee44ca6218df767c4
Austin Price <priceaustinw@gmail.com>
2018-04-22 12:32:34 -0400

    Switched rewrite actions to trigger on key-up instead of key-press

--- app/components/text-input-percent.js
+++ app/components/text-input-percent.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
+import { observer } from '@ember/object';
 
 export default Ember.Component.extend({
+
+  // init() {
+  //   this._super(...arguments);
+  //
+  //   const value = this.get('value');
+  //   this.set('percent', value);
+  // },
+  //
+  // triggerRewrite: observer('percent', function() {
+  //   this.set('value', this.get('percent'));
+  //   this.get('keypressAction')();
+  // }),
+
   actions: {
     keypressAction() {
       this.get('keypressAction')();
--- app/components/text-input-pixels.js
+++ app/components/text-input-pixels.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
+import { observer } from '@ember/object';
 
 export default Ember.Component.extend({
+
+  // init() {
+  //   this._super(...arguments);
+  //
+  //   const value = this.get('value');
+  //   this.set('pixel', value);
+  // },
+  //
+  // triggerRewrite: observer('pixel', function() {
+  //   this.set('value', this.get('pixel'));
+  //   this.get('keypressAction')();
+  // }),
 
   actions: {
     keypressAction() {
